0f51843781aad4e0f74ae45a827926406977bccf
Micah Taylor <micah@ted.com>
2021-08-27 17:24:47 -0500

    Data placeholder and stubbed out fetch user flow

--- client/src/components/patientData.tsx
+++ client/src/components/patientData.tsx
@@ -3,8 +3,16 @@ import { Button } from '@cmsgov/design-system';
 import eob from '../data/eob.json'
 
 export default function PatientData({ }) {
-
-    const [eob, setEob] = useState('No Medicare data added.')    
+    const [eob, setEob] = useState('No Medicare data added.')
+    //TODO: Fetch and return data 
+    // useEffect(() => {
+    //     fetch("../data/eob.json")
+    //     .then(res => res.json())
+    //     .then(data => {
+    //         console.log(data)
+    //         setEob(data);
+    //     })
+    // },[])      
     return (
         <div className="">
                         <h3>Medicare records</h3>
@@ -12,6 +20,10 @@ export default function PatientData({ }) {
 
             <h4>{ eob }</h4>
             <Button variation="primary" onClick={() => setEob('Data retreived')}>Authorize</Button>
+            {/* 
+            Button to fetch data and notify of success after auth flow
+            {eob && <Button variation="primary" onClick={() => setEob(eob)}>Authorize</Button>} 
+            */}
         </div>
     );
 };
