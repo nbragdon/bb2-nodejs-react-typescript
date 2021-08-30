ac038105c84c7c75b97fdd507ab2cc0c4cdc88ff
Micah Taylor <micahtaylor@Micah-Taylor-Macbook.local>
2021-08-30 17:53:45 -0500

    Layout, content, and images. Logging data in console.

--- client/src/components/patient.tsx
+++ client/src/components/patient.tsx
@@ -1,15 +1,17 @@
+import avatar from '../images/patient.png'
+
 export default function Patient() {
     return (
-        <div className="">
+        <div>
             <h3>Clinic records</h3>
-            <ul>
-                <li>
-                    <img src="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939" alt="Profile avatar" />
-                </li>
-                <li>Homer J. Simpson</li>
-                <li>Springfield General Hospital</li>
-                <li>Dr. Hibbert</li>
-            </ul>
+            <div className='ds-u-display--flex ds-u-flex-direction--row ds-u-align-items--center'>
+                <img src={avatar} alt="Profile avatar"/>
+                <ul>
+                    <li>John Doe</li>
+                    <li>Springfield General Hospital</li>
+                    <li>Dr. Hibbert</li>
+                </ul>
+            </div>
         </div>
     );
 };
--- client/src/components/patientData.tsx
+++ client/src/components/patientData.tsx
@@ -1,29 +1,36 @@
 import React, { useState, useEffect } from 'react'
 import { Button } from '@cmsgov/design-system';
-import eob from '../data/eob.json'
+// import eob from '../data/eob.json'
+import chart from '../images/who-charted.png'
 
 export default function PatientData({ }) {
-    const [eob, setEob] = useState('No Medicare data added.')
+    const [eob, setEob] = useState('Add your Medicare data')
     //TODO: Fetch and return data 
-    // useEffect(() => {
-    //     fetch("../data/eob.json")
-    //     .then(res => res.json())
-    //     .then(data => {
-    //         console.log(data)
-    //         setEob(data);
-    //     })
-    // },[])      
+    useEffect(() => {
+        fetch("../data/eob.json")
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            // setEob(data);
+        })
+    },[])      
     return (
-        <div className="">
-                        <h3>Medicare records</h3>
-            <p>Homer, you can now allow Springfield General Hospital access to your Medicare healthcare records!</p>
-
-            <h4>{ eob }</h4>
-            <Button variation="primary" onClick={() => setEob('Data retreived')}>Authorize</Button>
-            {/* 
-            Button to fetch data and notify of success after auth flow
-            {eob && <Button variation="primary" onClick={() => setEob(eob)}>Authorize</Button>} 
-            */}
+        <div>
+            <h3>Medicare records</h3>
+            <div className="ds-u-display--flex ds-u-flex-direction--row ds-u-align-items--start">
+                <img src={chart} alt="Chart icon" className=""/>
+                <p className='ds-u-padding-x--2 ds-u-margin-top--0'>
+                    John, you can now allow Springfield General Hospital access to your Medicare healthcare records!
+                </p>
+            </div>
+            <div className='ds-u-margin-top--2 ds-u-border-top--2'>
+                <h4>{ eob }</h4>
+                <Button variation="primary" onClick={() => setEob('Your data was retreived')}>Authorize</Button>
+                {/*
+                Button to fetch data and notify of success after auth flow
+                {eob && <Button variation="primary" onClick={() => setEob(eob)}>Authorize</Button>}
+                */}
+            </div>
         </div>
     );
 };
