1514804928c65cbc7446f788eee9f20dc9219d12
bfausett77 <brady.fausett@adhocteam.us>
2021-09-28 15:39:51 -0600

    Another comment fix

--- client/src/components/patient.tsx
+++ client/src/components/patient.tsx
@@ -4,7 +4,7 @@ export default function Patient() {
 
     /* DEVELOPER NOTES:
     * Here we are hard coding the users information for the sake of saving time
-    * you would display user information that you have stored in whatever persistence layer/mechcanism 
+    * you would display user information that you have stored in whatever persistence layer/mechanism 
     * your application is using
     */
     return (
